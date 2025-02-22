82283895e91f49b03420158bedc7b67423455940
TharHtet236401 <tharhteta3@gmail.com>
2025-02-22 16:56:29 +0700

    Display transactions on home page

--- transaction/views.py
+++ transaction/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from .models import Transaction
 # Create your views here.
 def home(request):
-    return render(request, 'transaction/home.html')
+    transactions = Transaction.objects.all()
+    context = {
+        'transactions': transactions
+    }
+    return render(request, 'transaction/home.html', context)
 
 def add_transaction(request):
     return render(request, 'transaction/add_transaction.html')
