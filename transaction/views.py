82a369979c40ca1cdba3f10be17f4fc9a3746bfe
TharHtet236401 <tharhteta3@gmail.com>
2025-02-22 17:12:12 +0700

    Implement pagination for transactions on home page

--- transaction/views.py
+++ transaction/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from .models import Transaction
+from django.core.paginator import Paginator
 # Create your views here.
 def home(request):
-    transactions = Transaction.objects.all()
+    transactions = Transaction.objects.all().order_by('-date')
     context = {
         'transactions': transactions
     }
+    paginator = Paginator(transactions, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context['page_obj'] = page_obj
     return render(request, 'transaction/home.html', context)
 
 def add_transaction(request):
